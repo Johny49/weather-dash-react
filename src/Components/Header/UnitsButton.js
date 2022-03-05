e070fad9f2b75de0152af7f54533625bc99bfa14
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-03-05 12:09:50 -0600

    added units toggle button and save to localStorage

---
+++ src/Components/Header/UnitsButton.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const UnitsButton = () => {
+  //  retrieve preferred units from localStorage if exists
+  const [unitType, setUnitType] = useState(
+    localStorage.getItem("saved-units") || "imperial"
+  );
+
+  const setUnitsHandler = () => {
+    if (unitType === "imperial") {
+      setUnitType("metric");
+      // save unit to localStorage
+      localStorage.setItem("saved-units", "metric");
+    } else {
+      setUnitType("imperial");
+      // save unit to localStorage
+      localStorage.setItem("saved-units", "imperial");
+    }
+    console.log(unitType);
+  };
+
+  return (
+    <Button variant="primary" onClick={setUnitsHandler}>
+      {unitType}
+    </Button>
+  );
+};
+
+export default UnitsButton;
